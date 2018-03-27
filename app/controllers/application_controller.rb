eb21591c846fa7d37f75bc1c30985515fbfdb394
Eden Mesfun <emesfun@gmail.com>
2018-03-27 20:47:20 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -54,10 +54,10 @@ class ApplicationController < Sinatra::Base
   delete '/posts/:id/delete' do
 
     @post = Post.all
+        binding.pry
     if @post.includes(:name => params[:name], content: params[:content])
       @post.destroy(:name => params[:name], :content => params[:content])
     end
-    binding.pry
 
     erb :delete
   end
