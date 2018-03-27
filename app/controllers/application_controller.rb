f162a53fe7bcedb26b44ffb5de1c102c1eb4adfb
Eden Mesfun <emesfun@gmail.com>
2018-03-27 21:32:49 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -48,7 +48,7 @@ class ApplicationController < Sinatra::Base
     if @post.includes(:name => params[:name], content: params[:content])
       @post.update(:name => params[:name], :content => params[:content])
     end
-    redirect to ('/posts/id')
+    redirect to ('/posts/2')
   end
 
   delete '/posts/:id/delete' do
