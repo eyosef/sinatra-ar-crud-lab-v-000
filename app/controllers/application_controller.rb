4fc27d34e7dd9dd0ea9393739a27b9c554f16919
Eden Mesfun <emesfun@gmail.com>
2018-03-27 21:44:01 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -45,7 +45,7 @@ class ApplicationController < Sinatra::Base
 
   patch "/update" do
     @post = Post.all
-    if @post.includes(:name => params[:name], content: params[:content])
+    if @post.!includes(:name => params[:name], content: params[:content])
       @post.update(:name => params[:name], :content => params[:content])
     end
 
